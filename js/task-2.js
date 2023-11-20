b0cfbfb68fd374e948f35604ccdaea2f688aa5eb
Vitaliy <131911773+whyzzz20@users.noreply.github.com>
2023-11-20 19:18:42 +0100

    allinone

---
+++ js/task-2.js
@@ -0,0 +1,38 @@
+function calcAverageCalories(days) {
+    if (days.length === 0) {
+    return 0;
+    }
+
+    const totalCalories = days.reduce((acc, day) => acc + day.calories, 0);
+
+    const averageCalories = totalCalories / days.length;
+
+    return Math.round(averageCalories);
+}
+
+console.log(
+    calcAverageCalories([
+        { day: "monday", calories: 3010 },
+        { day: "tuesday", calories: 3200 },
+        { day: "wednesday", calories: 3120 },
+        { day: "thursday", calories: 2900 },
+        { day: "friday", calories: 3450 },
+        { day: "saturday", calories: 3280 },
+        { day: "sunday", calories: 3300 }
+    ])
+);
+console.log(
+    calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+    ])
+);
+
+console.log(
+    calcAverageCalories([])
+);
